0b3a73c7a3b3128ac1bd49dd4c368a2c9b948e28
EduardoRFS <theeduardorfs@gmail.com>
2020-05-23 15:21:35 -0300

    initial commit

---
+++ install-gcc-workaround.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+set -e
+set -u
+
+# fix related to -fcommon on GCC 10 on Linux
+if [ "$(uname)" == 'Linux' ]; then
+    SH=$(which sh)
+    CC=$(which gcc)
+
+    MAJOR_VERSION_TO_PATCH="10"
+    MAJOR_VERSION=$($CC -dumpversion | awk -F. '{ print $1 }' )
+
+    if [ $MAJOR_VERSION -ge $MAJOR_VERSION_TO_PATCH ]; then
+        echo "#! $SH" >> $cur__bin/gcc
+        echo "eval $CC -fcommon '\"\$@\"'" >> $cur__bin/gcc
+        chmod +x $cur__bin/gcc
+    fi
+fi
